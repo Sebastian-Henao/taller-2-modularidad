736987772e191c52efaa6bc751e20a911470f7ca
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-03 17:36:57 -0500

    feat(marvel-rivals): Comsumo de datos desde el core

--- src/app/modules/module-marvel-rivals/pages/list-module-marvel-rivals/list-module-marvel-rivals.component.ts
+++ src/app/modules/module-marvel-rivals/pages/list-module-marvel-rivals/list-module-marvel-rivals.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MarvelRivals } from '../../interfaces/module-marvel-rivals.interface';
+import { ModuleMarvelRivalsService } from '../../services/module-marvel-rivals.service';
 
 @Component({
   selector: 'app-list-module-marvel-rivals.component',
@@ -7,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './list-module-marvel-rivals.component.scss',
 })
 export class ListModuleMarvelRivalsComponent {
-
+  moduleMarvelRivals: MarvelRivals[] = [];
+  private moduleMarvelRivalsService = inject(ModuleMarvelRivalsService);
+  ngOnInit(): void {
+    this.moduleMarvelRivalsService.getAllRivals().subscribe({
+      next: (moduleMarvelRivals) => this.moduleMarvelRivals = moduleMarvelRivals,
+      error: (error) => console.error(error),
+    });
+  }
 }
