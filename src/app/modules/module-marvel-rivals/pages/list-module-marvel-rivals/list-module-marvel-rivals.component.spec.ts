18625ca46d2c1e28fd7dd18b5275d3051da585cf
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-03 19:01:03 -0500

    test: Se completan pruebas unitarias

--- src/app/modules/module-marvel-rivals/pages/list-module-marvel-rivals/list-module-marvel-rivals.component.spec.ts
+++ src/app/modules/module-marvel-rivals/pages/list-module-marvel-rivals/list-module-marvel-rivals.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListModuleMarvelRivalsComponent } from './list-module-marvel-rivals.component';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { ModuleMarvelRivalsService } from '../../services/module-marvel-rivals.service';
+import { TableModuleMarvelRivalsComponent } from '../../components/table-module-marvel-rivals/table-module-marvel-rivals.component';
+import { BadgeComponent } from '../../../shared/components/badge/badge.component';
+import { MARVEL_RIVALS_DATA } from '../../../../core/config/marvel-rivals.config';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('ListModuleMarvelRivalsComponent', () => {
   let component: ListModuleMarvelRivalsComponent;
   let fixture: ComponentFixture<ListModuleMarvelRivalsComponent>;
+  let moduleMarvelRivalsService: ModuleMarvelRivalsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ListModuleMarvelRivalsComponent]
+      declarations: [ListModuleMarvelRivalsComponent, TableModuleMarvelRivalsComponent, BadgeComponent]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ListModuleMarvelRivalsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    moduleMarvelRivalsService = TestBed.inject(ModuleMarvelRivalsService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deberia llamar a getAllRivals al iniciar', () => {
+    const spyGetAllRivals = jest.spyOn(moduleMarvelRivalsService, 'getAllRivals').mockReturnValue(of(MARVEL_RIVALS_DATA));
+    fixture.detectChanges();
+    expect(spyGetAllRivals).toHaveBeenCalled();
+  });
+
+  it('Deberia asignar los personajes de Marvel Rivals recibidos del servicio', () => {
+    jest.spyOn(moduleMarvelRivalsService, 'getAllRivals').mockReturnValue(of(MARVEL_RIVALS_DATA));
+    fixture.detectChanges();
+    expect(component.moduleMarvelRivals).toEqual(MARVEL_RIVALS_DATA);
+  });
+
+  it('Deberia pasar los personajes de Marvel Rivals al componente table-module-marvel-rivals', () => {
+    jest.spyOn(moduleMarvelRivalsService, 'getAllRivals').mockReturnValue(of(MARVEL_RIVALS_DATA));
+    fixture.detectChanges();
+    const tableComponent = fixture.debugElement
+      .query(By.directive(TableModuleMarvelRivalsComponent))
+      .componentInstance;
+    expect(tableComponent.moduleMarvelRivals).toEqual(MARVEL_RIVALS_DATA);
+  });
+
+  it('debería manejar el error cuando falla getAllRivals', () => {
+    component.moduleMarvelRivals = [];
+    const errorResponse = new Error('Error al cargar personajes de Marvel Rivals');
+  
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+    jest.spyOn(moduleMarvelRivalsService, 'getAllRivals').mockReturnValue(throwError(() => errorResponse));
+  
+    fixture.detectChanges();
+  
+    expect(moduleMarvelRivalsService.getAllRivals).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(errorResponse);
+    expect(component.moduleMarvelRivals.length).toBe(0);
+    });
 });
--- src/app/modules/module-marvel-rivals/services/module-marvel-rivals.service.spec.ts
+++ src/app/modules/module-marvel-rivals/services/module-marvel-rivals.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ModuleMarvelRivalsService } from './module-marvel-rivals.service';
+import { MARVEL_RIVALS_DATA } from '../../../core/config/marvel-rivals.config';
 
 describe('ModuleMarvelRivalsService', () => {
   let service: ModuleMarvelRivalsService;
@@ -12,5 +13,13 @@ describe('ModuleMarvelRivalsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getAllRivals debería retornar un observable con los personajes', (done) => {
+    service.getAllRivals().subscribe(rivals => {
+      expect(rivals).toEqual(MARVEL_RIVALS_DATA);
+      expect(rivals.length).toBe(MARVEL_RIVALS_DATA.length);
+      done();
+    });
   });
 });
